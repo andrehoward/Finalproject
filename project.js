658fbcf97ca9865080a2ab03e0c92985a07e876b
andrehoward <andre.howard40@yahoo.com>
2016-07-20 19:04:02 -0400

    project

---
+++ project.js
@@ -0,0 +1,9 @@
+var person = prompt("Please enter your name", "Andre' Howard");
+
+// add response to enter name. maybe hello name + welcome
+
+alert("Hello\nHow are you today?");
+
+//add response to hello how are you
+
+
---
+++ public/main.js
@@ -0,0 +1,34 @@
+
+var update = document.getElementById('update')
+var del = document.getElementById('delete')
+
+update.addEventListener('click', function () {
+  fetch('quotes', {
+    method: 'put',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      'name': 'Patsy Mozee',
+      'quote': 'What you bringing me back from the store?'
+    })
+  })
+  .then(response => {
+    if (response.ok) return response.json()
+  })
+  .then(data => {
+    console.log(data)
+  })
+})
+
+del.addEventListener('click', function () {
+  fetch('quotes', {
+    method: 'delete',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      'name': 'Patsy Mozee'
+    })
+  }).then(function (response) {
+    window.location.reload()
+  })
+})
